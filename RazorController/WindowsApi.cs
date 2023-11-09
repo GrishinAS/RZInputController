40f1936d693e2890f1d0901ea1139a049597e7cd
GrishinAS <anton.grishin@mts.ru>
2023-11-08 22:38:18 -0800

    Fix universal charset DllImport

--- RazorController/WindowsApi.cs
+++ RazorController/WindowsApi.cs
@@ -6,12 +6,12 @@ internal class WindowsApi
 {
     private const string Kernel32 = "kernel32.dll";
     
-    [DllImport(Kernel32, SetLastError = true, CharSet = CharSet.Unicode)]
+    [DllImport(Kernel32, SetLastError = true)]
     public static extern IntPtr LoadLibrary(string lpLibFileName);
     
     [DllImport(Kernel32, SetLastError = true)]
     public static extern bool FreeLibrary(IntPtr hLibModule);
     
-    [DllImport(Kernel32, SetLastError = true, CharSet = CharSet.Unicode)]
+    [DllImport(Kernel32, SetLastError = true)]
     public static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
 }
